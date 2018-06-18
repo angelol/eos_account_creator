643a5e4a36a12892786c11eb2fcd1dd6fa36023d
Angelo Laub <angelolaub@gmail.com>
2018-06-18 10:18:56 +0200

    always use the same public endpoint

--- buy/gen_account.js
+++ buy/gen_account.js
@@ -1,7 +1,7 @@
 let Eos = require('eosjs');
 let fs = require('fs');
 
-let httpEndpoint = 'http://api.eosnewyork.io';
+let httpEndpoint = 'https://publicapi-mainnet.eosauthority.com';
 let chainId = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
 
 creator = 'accountcreat';
