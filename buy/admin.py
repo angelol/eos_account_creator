b3228355427299912cabf1c74b2ba48bc2a8cc98
Angelo Laub <angelolaub@gmail.com>
2018-06-19 18:23:23 +0200

    admin

--- buy/admin.py
+++ buy/admin.py
@@ -7,3 +7,5 @@ admin.site.disable_action('delete_selected')
 class PurchaseAdmin(admin.ModelAdmin):
     list_display = ('created_at', 'account_name', 'payment_received', 'account_created')
     ordering = ('-created_at', )
+    fields = ('account_name', 'public_key', 'payment_received', 'account_created', 'coinbase_code', 'price_usd', 'currency')
+    readonly_fields = fields
