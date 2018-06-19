8662a762d09fb26fd464d2db9cc01beb3a3786c5
Angelo Laub <angelolaub@gmail.com>
2018-06-19 18:18:29 +0200

    admin.site.disable_action('delete_selected')

--- buy/admin.py
+++ buy/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from buy.models import Purchase
 
+admin.site.disable_action('delete_selected')
+
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
     list_display = ('created_at', 'account_name', 'payment_received', 'account_created')
