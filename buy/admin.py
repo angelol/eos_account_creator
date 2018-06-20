34af828ab4a54bbfb7dd86d1ceedb32be7e22682
Angelo Laub <angelolaub@gmail.com>
2018-06-20 09:56:26 +0200

    admin

--- buy/admin.py
+++ buy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from buy.models import Purchase
+from buy.models import Purchase, PriceData
 
 admin.site.disable_action('delete_selected')
 
@@ -9,3 +9,11 @@ class PurchaseAdmin(admin.ModelAdmin):
     ordering = ('-created_at', )
     fields = ('account_name', 'public_key', 'payment_received', 'account_created', 'coinbase_code', 'price_usd', 'currency', 'stripe', 'coinbase')
     readonly_fields = fields
+    
+@admin.register(PriceData)
+class PriceDataAdmin(admin.ModelAdmin):
+    list_display = ('updated_at', 'eos_usd', 'ram_kb_eos', 'ram_kb_usd')
+    readonly_fields = list_display
+    
+    def ram_kb_usd(self, instance):
+        return PriceData.ram_kb_usd()
