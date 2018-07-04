4e1bb0c03649eff06aed42f846b7ffa38a9f0ac8
Angelo Laub <angelolaub@gmail.com>
2018-07-04 16:06:07 +0500

    admin

--- buy/admin.py
+++ buy/admin.py
@@ -2,9 +2,75 @@ from django.contrib import admin
 from buy.models import Purchase, PriceData, CoinbaseEvent
 import json
 from decimal import Decimal
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
+import calendar
 
 admin.site.disable_action('delete_selected')
 
+class DateListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Date'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'date'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        from datetime import date
+        return (
+            ('today', 'Today'),
+            ('yesterday', 'Yesterday'),
+            ('day_before_yesterday', 'Day before yesterday'),
+            ('past_7_days', 'This week'),
+            ('this_month', "This Month"),
+            ('last_month', "Last Month"),
+            ('month_before_last_month', "Month before last Month"),
+            
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        now = timezone.now()
+        # When time zone support is enabled, convert "now" to the user's time
+        # zone so Django's definition of "Today" matches what the user expects.
+        if timezone.is_aware(now):
+            now = timezone.localtime(now)
+
+        today = now.date()
+
+        def last_month_range(today):
+            first_of_current_month = today.replace(day=1)
+            first_of_last_month = first_of_current_month - relativedelta(months=1)
+            last_of_last_month = first_of_last_month.replace(day=calendar.monthrange(first_of_last_month.year, first_of_last_month.month)[1])
+            return (first_of_last_month, last_of_last_month)
+
+        def month_before_last_month(today):
+            first_of_last_month, last_of_last_month = last_month_range(today)
+            return (first_of_last_month - relativedelta(months=1), first_of_last_month - relativedelta(days=1))
+
+        return {
+            None: queryset,
+            'today': queryset.filter(created_at__date=today),
+            'yesterday': queryset.filter(created_at__date=today-relativedelta(days=1)),
+            'day_before_yesterday': queryset.filter(created_at__date=today-relativedelta(days=2)),
+            'past_7_days': queryset.filter(created_at__date__range=(today-relativedelta(days=7), today)),
+            'this_month': queryset.filter(created_at__date__range=(today.replace(day=1), today)),
+            'last_month': queryset.filter(created_at__date__range=last_month_range(today)),
+            'month_before_last_month': queryset.filter(created_at__date__range=month_before_last_month(today)),
+        }[self.value()]
+        
 def process(modeladmin, request, queryset):
     for x in queryset:
         x.complete_purchase_and_save()
@@ -19,7 +85,7 @@ class PurchaseAdmin(admin.ModelAdmin):
     readonly_fields = fields
     actions = [process]
     search_fields = ('account_name', 'owner_key', 'active_key')
-    list_filter = ('payment_received', 'account_created', 'created_at')
+    list_filter = ('payment_received', 'account_created', DateListFilter)
     
     def price(self, instance):
         if instance.price_cents:
