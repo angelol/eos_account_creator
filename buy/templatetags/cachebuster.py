09f5913d621b550d145198385b8000bab92472cb
Angelo Laub <angelolaub@gmail.com>
2018-07-18 23:15:44 +0200

    cachebuster only in debug mode

--- buy/templatetags/cachebuster.py
+++ buy/templatetags/cachebuster.py
@@ -1,8 +1,12 @@
 from time import time
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 @register.simple_tag
 def cachebuster():
-    return str(time())
+    if settings.DEBUG:
+        return "?" + str(time())
+    else:
+        return ""
