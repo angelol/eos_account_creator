c24417c7803c58c4c3f4856d5a5381080fad9003
Angelo Laub <angelolaub@gmail.com>
2018-07-19 00:44:08 +0200

    price

--- buy/templatetags/tags.py
+++ buy/templatetags/tags.py
@@ -1,6 +1,7 @@
 from time import time
 from django import template
 from django.conf import settings
+from buy.models import Purchase
 
 register = template.Library()
 
@@ -10,3 +11,8 @@ def cachebuster():
         return "?" + str(time())
     else:
         return ""
+        
+@register.simple_tag
+def price():
+    return '%.2f' % Purchase.get_prices_usd()[1]
+    
